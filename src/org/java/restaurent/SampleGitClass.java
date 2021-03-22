a32be1a95e39565d08b07e1b10358d053c83bcc2
Nagarajudandu <nagarajudandu0249@gmail.com>
2021-03-22 23:07:48 +0530

    Iam Added New Features Sucessfully..!

--- src/org/java/restaurent/SampleGitClass.java
+++ src/org/java/restaurent/SampleGitClass.java
@@ -5,6 +5,8 @@ public class SampleGitClass {
 	public static void main(String[] args) 
 	{
 		System.out.println("Git IS Sucessfully Completed..");
+		
+		System.out.println("Hello Nagaraju......");
 	}
 
 }
